5161f0486dda9ac08ba829bdb89ef9ed345fe9c9
Kashif Suhail <eeekashifsuhail@gmail.com>
2026-08-27 04:39:26 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Optional static configuration.
-// Leaving these blank lets you enter the values in the app.
-// Never put a Supabase service-role/secret key here.
-window.SUPABASE_URL = "";
-window.SUPABASE_PUBLISHABLE_KEY = "";
